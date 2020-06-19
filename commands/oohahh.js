ffa5171df5fa4f09be1fb06c179d5972d0aed937
JckD <c16335383@mydit.ie>
2020-06-19 02:07:07 +0100

    properly display memberlist

--- commands/oohahh.js
+++ commands/oohahh.js
@@ -10,14 +10,16 @@ module.exports = {
                 const totalOnline = fetchedMembers.filter(member => member.presence.status === 'online');
                 // We now have a collection with all online member objects in the totalOnline variable
                 const members = totalOnline.toJSON();
-
                 const users = members.map(member => member.displayName);
-                // console.log(members);
-                message.channel.send(`${users} are in the RA!`);
+                const usersArray = Object(users);
+                if (usersArray.length == 1) {
+                    message.channel.send(`${users} is in the RA!`);
+                } 
+                else {
+                    message.channel.send(usersArray.slice(0, -1) + ' and ' + usersArray[usersArray.length - 1] + ' are in the RA!');
+                }
+                
             });
-            
-            // message.channel.send(`Up the Ra! ${message.guild.name}`);
-            
         }
 	},
 };
